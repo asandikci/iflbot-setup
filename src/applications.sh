55e5e7030a86d66a505da256c591767985140ba2
admin <github@aliberksandikci.com.tr>
2022-10-10 22:07:55 +0300

    Initial Commit

--- src/applications.sh
+++ src/applications.sh
@@ -28,3 +28,10 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt install code -y
 sudo apt update -y
 sudo apt upgrade -y
+
+#Chrome https://askubuntu.com/questions/510056/how-to-install-google-chrome
+wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
+gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update -y
+sudo apt-get install google-chrome-stable -y
