2ba7147bd1de223c339b6184c926c8f0591b264a
admin <github@aliberksandikci.com.tr>
2022-10-10 22:06:03 +0300

    Initial Commit

--- src/applications.sh
+++ src/applications.sh
@@ -2,10 +2,29 @@
 # setup script for applications
 
 #Sublime Text https://www.sublimetext.com/docs/linux_repositories.html
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get update -y
+sudo apt-get install sublime-text -y
 
-#
+#File-roller
+sudo apt install file-roller -y
+
+#VLC
+sudo apt-get install vlc -y
+
+#gcc
+sudo apt install gcc -y
+
+#Xournal++
+sudo apt update -y
+sudo apt install xournalpp -y
+
+#VS Code https://linuxize.com/post/how-to-install-visual-studio-code-on-ubuntu-20-04/
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+sudo apt install code -y
+sudo apt update -y
+sudo apt upgrade -y
