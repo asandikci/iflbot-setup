95b0e1090e897e72b7cef6a747553ca91ad0ea3e
asandikci <git@aliberksandikci.com.tr>
2023-10-23 17:06:05 +0300

    update dark theme

--- src/config.sh
+++ src/config.sh
@@ -7,5 +7,6 @@
 
 
 ### XFCE SETTINGS
+xfconf-query -c "xsettings" -p "/Net/ThemeName" -s "pardus-xfce-dark" --type "string" --create
 xfconf-query -c "xfwm4" -p "/general/theme" -s "pardus-xfce-dark" --type "string" --create
 xfconf-query -c "xsettings" -p "/Net/IconThemeName" -s "pardus-xfce-dark" --type "string" --create
