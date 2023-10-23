1f596cb7ef8d33abd2ca45822386bd5fb6429119
asandikci <git@aliberksandikci.com.tr>
2023-10-23 17:26:15 +0300

    change panel size

--- src/user-config.sh
+++ src/user-config.sh
@@ -2,8 +2,15 @@
 # general configs
 
 ### XFCE SETTINGS
+
+# Dark Theme
 xfconf-query -c "xsettings" -p "/Net/ThemeName" -s "pardus-xfce-dark" --type "string" --create
 xfconf-query -c "xfwm4" -p "/general/theme" -s "pardus-xfce-dark" --type "string" --create
 xfconf-query -c "xsettings" -p "/Net/IconThemeName" -s "pardus-xfce-dark" --type "string" --create
+
+# Panel Size
+xfconf-query --channel 'xfce4-panel' --property '/panels/panel-1/size' --type int --set 28
+
+
 
 ### SUBLIME BUILDS
